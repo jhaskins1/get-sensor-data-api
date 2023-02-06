8318bba97b8718a68e63212ac7acd0feb1c2f8ec
jhaskins1 <55133298+jhaskins1@users.noreply.github.com>
2023-02-06 15:01:37 -0600

    initial

---
+++ src/main/java/ipro497970/middleware/getsensordataapi/GetSensorDataApiApplication.java
@@ -0,0 +1,13 @@
+package ipro497970.middleware.getsensordataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetSensorDataApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetSensorDataApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipro497970/middleware/getsensordataapi/GetSensorDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ipro497970.middleware.getsensordataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetSensorDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
